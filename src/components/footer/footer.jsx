22ba2f64c3d1d62ae83b8bfcbcb437af1d754f7e
Leo-apu <34524268@fi.unju.edu.ar>
2024-12-10 08:31:27 -0300

    add project

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import "./footer.css";
+
+function Footer() {
+    return (
+        <Fragment>
+            <footer className="footer">
+                <img src="/img/footer.png" alt="org-footer" />
+            </footer>
+        </Fragment>
+    );
+}
+
+export default Footer;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import "./header.css";
+
+function Header() {
+    return (
+        <Fragment>
+            <header className="header">
+                <img src="/img/header.png" alt="org" />
+            </header>
+        </Fragment>
+    );
+}
+
+export default Header;
